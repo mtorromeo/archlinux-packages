9c9bef248c1f39d639510b0482f4e2f08aabfb23
Massimiliano Torromeo <massimilianotorromeo@artera.it>
2015-07-24 17:02:47 +0200

    tshock.sh doesn't have to be yet another init script

--- tshock/tshock.sh
+++ tshock/tshock.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-case "$1" in
-    stop)
-        PID="$2"
-        tmux send-keys -t tshock-console e x i t ENTER
-        while [ -d /proc/$PID ]; do
-            sleep 1
-        done
-        ;;
-    *) exec sudo -u tshock tmux attach -t tshock-console
-esac
+exec sudo -u tshock tmux attach -t tshock-console
